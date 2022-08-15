ab916bf71021a0b627cecd7c472dda1b68ff984e
koreicnurs <koreicnurs@gmail.com>
2022-08-15 14:13:51 +0600

    add logic for get last 5 messages and read

--- app/messages.js
+++ app/messages.js
@@ -10,17 +10,27 @@ fs.readdir(path, (err, files) => {
     files.forEach(file => {
         messagesArray.push(path + '/' + file);
     });
+    if (messagesArray.length >= 5) {
+        messagesArray = messagesArray.slice(-5);
+    }
 })
 
 router.post('/', (req, res) => {
-    fs.writeFileSync(`./messages/${date.toISOString()}.json`, JSON.stringify({...req.body, datetime: date.toISOString()}));
+    fs.writeFileSync(`./messages/${date.toISOString()}.json`, JSON.stringify({
+        ...req.body,
+        datetime: date.toISOString()
+    }));
     res.send('Created');
 });
 
 router.get('/', (req, res) => {
-    res.send(fs.readFileSync(messagesArray[0], (err, f) => {
-        console.log(f);
-    }));
+    res.send(
+        messagesArray.map(m => {
+            return JSON.parse(fs.readFileSync(m, (err, f) => {
+                console.log(f)
+            }))
+        })
+    )
 });
 
 module.exports = router;

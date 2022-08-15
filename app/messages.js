0b478cac04521c3c9494d94047e87d08e59b51a6
koreicnurs <koreicnurs@gmail.com>
2022-08-15 12:45:32 +0600

    add get request for each obj messages from array

--- app/messages.js
+++ app/messages.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const fs = require('fs');
 const date = new Date();
 
-const fileMessageName = `../messages/m.json`;
+const fs = require('fs');
+const path = './messages';
+let messagesArray = [];
+
+fs.readdir(path, (err, files) => {
+    files.forEach(file => {
+        messagesArray.push(path + '/' + file);
+    });
+})
 
 router.post('/', (req, res) => {
-    fs.writeFileSync(`./messages/${date.toISOString()}.txt`, JSON.stringify({...req.body, datetime: date.toISOString()}));
+    fs.writeFileSync(`./messages/${date.toISOString()}.json`, JSON.stringify({...req.body, datetime: date.toISOString()}));
     res.send('Created');
 });
 
 router.get('/', (req, res) => {
-    res.send('Get messages');
+    res.send(fs.readFileSync(messagesArray[0], (err, f) => {
+        console.log(f);
+    }));
 });
 
 module.exports = router;
